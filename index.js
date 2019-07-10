c8e801bc23c95f4b0dfec76b2ee3239a148c20bf
Samiullah Khan <samiullah.khan.1989@gmail.com>
2019-07-10 15:30:12 +0400

    files are listed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const process = require('process');
 const path = require('path');
 const folderPath = '/Downloads/Video';
@@ -7,37 +9,41 @@ const folderPath = '/Downloads/Video';
 const port = 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
-app.get('/', (req,res) => res.render('index'))
+app.get('/', (req, res) => res.render('index'));
 
-app.get('/files', (req,res) => {
-  res.send('wheee')
-})
+const isFile = fileName => {
+  // console.log(fs.lstatSync(fileName))
+  return fs.lstatSync(fileName).isFile();
+};
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const isSrt = fileName => {
+  return path.extname(fileName) === '.srt';
+};
 
+app.post('/files', (req, res) => {
+  const { folderName } = req.body;
+  const list = fs
+    .readdirSync(folderPath)
+    .map(item => {
+      return path.join(folderPath, item);
+    })
+    .filter(isFile)
+    .filter(isSrt);
 
-// const isFile = fileName => {
-//   // console.log(fs.lstatSync(fileName))
-//   return fs.lstatSync(fileName).isFile()
-// }
+  res.json(list);
+});
 
-// const isSrt = fileName => {
-//   return (path.extname(fileName) === '.srt')
-// }
-
-// const list = fs.readdirSync(folderPath).map(item => {
-  
-//   return path.join(folderPath, item)
-// })
-// .filter(isFile)
-// .filter(isSrt)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // if(process.argv.includes('rename')){
 //   console.log('Renaming files');
-  
+
 // }
 
 // console.log(list);
--- public/index.js
+++ public/index.js
@@ -1,14 +1,31 @@
 const mainForm = document.querySelector('.main-form');
+const filesContainer = document.querySelector('.files');
+let ul = document.createElement('ul');
 
 mainForm.addEventListener('submit', e => {
   e.preventDefault();
-  fetch('/files')
-  .then(resp => resp.text())
-  .then(resp => {
-    console.log(resp);
+  const data = {
+    folderName: e.target.folderName.value
+  }
+  fetch('/files', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(resp => resp.json())
+  .then(files => {
+    for (let index = 0; index < files.length; index++) {
+      const path = files[index];
+      const el = document.createElement('li')
+      el.textContent = path
+      ul.appendChild(el)
+    }
+    console.log(files);
+    filesContainer.appendChild(ul);
   }).catch(error => {
     console.log(error);
     
   })
-  console.log(e.target);
 })
