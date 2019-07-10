08b53539624cd020792a20a22350f40ba83ad6c0
Samiullah Khan <samiullah.khan.1989@gmail.com>
2019-07-10 14:05:02 +0400

    added basic server

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const fs = require('fs');
+const express = require('express');
+const process = require('process');
 const path = require('path');
 const folderPath = '/Downloads/Video';
 
-const isFile = fileName => {
-  // console.log(fs.lstatSync(fileName))
-  return fs.lstatSync(fileName).isFile()
-}
+const port = 3000;
 
-const isSrt = fileName => {
-  return (path.extname(fileName) === '.srt')
-}
+const app = express();
+app.get('/', (req,res) => res.send('Hello World'))
 
-const list = fs.readdirSync(folderPath).map(item => {
-
-  return path.join(folderPath, item)
-})
-.filter(isFile)
-.filter(isSrt)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-console.log(list);
+// const isFile = fileName => {
+//   // console.log(fs.lstatSync(fileName))
+//   return fs.lstatSync(fileName).isFile()
+// }
+
+// const isSrt = fileName => {
+//   return (path.extname(fileName) === '.srt')
+// }
+
+// const list = fs.readdirSync(folderPath).map(item => {
+  
+//   return path.join(folderPath, item)
+// })
+// .filter(isFile)
+// .filter(isSrt)
+
+// if(process.argv.includes('rename')){
+//   console.log('Renaming files');
+  
+// }
+
+// console.log(list);
